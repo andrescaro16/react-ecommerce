7f3abd6b9365c99d0e6283427889659bc6956f22
andrescaro16 <julicaro2003@gmail.com>
2023-04-07 15:13:00 -0500

    Homepage with components and sanity for the backend

---
+++ sanity-ecommerce/schemas/index.js
@@ -0,0 +1,8 @@
+//index.ts
+//import createSchema from 'part:@sanity/base/schema-creator';
+//import schemaTypes from 'all:part:@sanity/base/schema-type';
+
+import product from './product';
+import banner from './banner';
+
+export const schemaTypes = [product, banner]
